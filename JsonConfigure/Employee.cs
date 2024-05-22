01c3d0585eb6b04b6bac4a83f1b70f27065668f2
Shahrriar Haque <shahriarhaque445@gmail.com>
2024-05-22 16:21:59 +0600

    Add project files.

---
+++ JsonConfigure/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JsonConfigure
+{
+    public class Employee
+    {
+        [JsonInclude] // add this key to json file 
+        public string Email { get; set; }
+
+
+        [JsonIgnore]// for ignore the id to add in json file 
+        public int Id { get; set; }
+        [JsonPropertyName("Full Name")] // for changing the key name
+        public string Name { get; set; }
+            public string Description { get; set; }
+        public Address Address { get; set; }
+    }
+    public class Address
+    {
+        public string Street { get; set; }
+            public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
+    }
+}
---
+++ JsonConfigure/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using JsonConfigure;
+using System.Text.Json;
+
+var employee = new Employee()
+{
+    Id = 1,
+    Name = "sha",
+    Description = "sdfasdf",
+    Address = new Address()
+    {
+        State = "123 st",
+        City = "Dhaka",
+        Street = "mo",
+        PostalCode = "343"
+    }
+};
+
+// Serialization -- convert c# json file 
+// Serialize - process is converting c# object to json
+var options=new JsonSerializerOptions { WriteIndented = true };
+var json = JsonSerializer.Serialize(employee,options);
+
+File.WriteAllText("C:\\Users\\Hameem\\Desktop\\Intern - Shahriar Haque (Shipon)\\C# Console\\JsonConfigure\\JsonConfigure\\employee.json", json); // Save all the file to json file 
+Console.WriteLine(json);
